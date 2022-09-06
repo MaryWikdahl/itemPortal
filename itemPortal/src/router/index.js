b2948fdb6c9abac13f27ac365ee1569a8454e5f1
Julia Wikdahl <julia_2000@live.se>
2022-09-06 10:40:43 +0200

    #Routing added all the views

--- itemPortal/src/router/index.js
+++ itemPortal/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import homePage from '../views/homePage.vue'
+import loginPage from '../views/loginPage.vue'
+import itemPage from '../views/itemPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,15 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: homePage
+    },
+    {
+      path:'/login',
+      component: loginPage,
+    },
+
+    {
+      path: '/item',
+      component: itemPage
     }
    
   ]
